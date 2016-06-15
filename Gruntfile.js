faffab26f244e4c7f13ff4ebbab891acbd58eb94
Savannah Henderson <savannahjune@gmail.com>
2016-06-14 22:42:49 -0700

    now lists title and dates of events in chronological order, adds sass, grunt, underscore and moment as dependencies `

---
+++ Gruntfile.js
@@ -0,0 +1,26 @@
+module.exports = function(grunt) {
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
+		sass: {
+			dist: {
+				files: {
+					'style/style.css' : 'style/style.scss'
+				}
+			}
+		},
+		watch: {
+      main: {
+        options: {
+          livereload: true
+        }
+      },
+			css: {
+				files: '**/*.scss',
+				tasks: ['sass']
+			}
+		}
+	});
+	grunt.loadNpmTasks('grunt-contrib-sass');
+	grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.registerTask('default',['watch']);
+};
--- events.service.js
+++ events.service.js
@@ -1,11 +1,18 @@
-$.getJSON( "ajax/test.json", function( data ) {
-  var items = [];
-  $.each( data, function( key, val ) {
-    items.push( "<li id='" + key + "'>" + val + "</li>" );
-  });
+$(document).ready(function () {
 
-  $( "<ul/>", {
-    "class": "my-new-list",
-    html: items.join( "" )
-  }).appendTo( "body" );
+  $.getJSON( "https://s3.amazonaws.com/interview-api-samples/events-results.json", function( data ) {
+    var events, eventElements = [], chronologicalEvents;
+    events = data.events;
+    chronologicalEvents = _.sortBy(events, 'startDate');
+    _.each(chronologicalEvents, function(event) {
+      eventElements.push( "<div class='event-wrapper'> <div class='event-name'>" + event.name + "</div>" +
+                          // "<div class='event-time'>" + new Date(event.startDate).toLocaleString() + "</div>");
+                          "<div class='event-time'>" + moment(event.startDate).format('LLLL') + "</div></div>");
+    });
+
+    $( "<div/>", {
+      "id": "event-list-wrapper",
+      html: eventElements.join( "" )
+    }).appendTo( "body" );
+  });
 });
