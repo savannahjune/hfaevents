24cddf9e71eeb780a1564ffe4cff62e85b6a84e7
Savannah Henderson <savannahjune@gmail.com>
2016-06-15 21:46:09 -0700

    improves code quality and adds attending badges to events

--- events.service.js
+++ events.service.js
@@ -1,9 +1,11 @@
 $(document).ready(function () {
+  var eventTemplateSource, eventItemTemplate,
+  eventURL = 'https://s3.amazonaws.com/interview-api-samples/events-results.json';
 
-  var event_listitem_template_source   = $("#entry-template").html();
-  var event_listitem_template = Handlebars.compile(event_listitem_template_source);
+  eventTemplateSource = $("#event-template").html();
+  eventItemTemplate = Handlebars.compile(eventTemplateSource);
 
-  $.getJSON( "https://s3.amazonaws.com/interview-api-samples/events-results.json", function( data ) {
+  $.getJSON(eventURL, function(data) {
     var events, eventElements = [], chronologicalEvents;
     events = data.events;
     chronologicalEvents = _.sortBy(events, 'startDate');
@@ -11,8 +13,8 @@ $(document).ready(function () {
 
       var context = event;
       context.startDatePretty =  moment(event.startDate).format('LLLL');
-      var event_item_html    = event_listitem_template(context);
-      eventElements.push(event_item_html);
+      var eventItemHTML = eventItemTemplate(context);
+      eventElements.push(eventItemHTML);
 
     });
 
